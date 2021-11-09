eb2d7ec7e7a5913ac73189a1e4d5993ea6d08022
Melanie Arellano <melaniesigrid@protonmail.com>
2021-11-09 11:24:43 -0600

    Add event listeners

--- menu-bottom.js
+++ menu-bottom.js
@@ -2,7 +2,9 @@ const mainMenu = document.querySelector('.mainMenu');
 const closeMenu = document.querySelector('.closeMenu');
 const openMenu = document.querySelector('.openMenu');
 
-
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+mainMenu.addEventListener('click', close);
 
 function show() {
     mainMenu.style.display = 'flex';
