1195899d646af47785a2b80eedb1b7f1e1413000
joseheco <jferhc@gmail.com>
2021-11-11 11:52:27 -0500

    correct linter problems

--- menu-bottom.js
+++ menu-bottom.js
@@ -2,36 +2,34 @@ const mainMenu = document.querySelector('.mainMenu');
 const closeMenu = document.querySelector('.closeMenu');
 const openMenu = document.querySelector('.openMenu');
 
+function show() {
+  mainMenu.style.display = 'flex';
+  mainMenu.style.top = '0';
+}
+
+function close() {
+  mainMenu.style.top = '-100%';
+}
 
 openMenu.addEventListener('click', show);
 closeMenu.addEventListener('click', close);
 mainMenu.addEventListener('click', close);
-
-
-function show() {
-    mainMenu.style.display = 'flex';
-    mainMenu.style.top = '0';
-}
-
-function close() {
-    mainMenu.style.top = '-100%'
-}
 
 const popUp = document.querySelector('.popup');
 const openModal = document.querySelector('.button-project');
 const closeModal = document.querySelector('.closePop');
 const overlay = document.querySelector('#overlay');
 
-openModal.addEventListener('click', showPop);
-closeModal.addEventListener('click', closePop);
-
 function showPop() {
-popUp.style.display = 'flex';
-popUp.style.top = '0';
-overlay.classList.add('active');
+  popUp.style.display = 'flex';
+  popUp.style.top = '0';
+  overlay.classList.add('active');
 }
 
 function closePop() {
-popUp.style.top = '-150%'
-overlay.classList.remove('active');
+  popUp.style.top = '-150%';
+  overlay.classList.remove('active');
 }
+
+openModal.addEventListener('click', showPop);
+closeModal.addEventListener('click', closePop);
