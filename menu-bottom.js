e21e5653349f48fe457aef45d3d84ef570d2c5bd
Melanie Arellano <melaniesigrid@protonmail.com>
2021-11-11 16:25:31 -0600

    Add email validation skeleton

--- menu-bottom.js
+++ menu-bottom.js
@@ -33,3 +33,17 @@ function closePop() {
 
 openModal.addEventListener('click', showPop);
 closeModal.addEventListener('click', closePop);
+
+const form = document.querySelector('');
+const email = document.getElementById('');
+const error = document.querySelector('');
+const regExp = /^[a-z0-9_-]+@[a-z0-9]+\.[a-z]+\.?[a-z]+/g;
+
+form.addEventListener('submit', (event) => {
+if (regExp.test(email.value)) {
+error.innerHTML = '';
+} else {
+event.preventDefault();
+error.innerHTML = '';
+}
+});
